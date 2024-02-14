a9b3a9d47645fa5c760dcdd8b77ee98051c49717
everysun <everysun@amuz.co.kr>
2024-02-14 12:07:35 +0900

    chore: 함수 추가, 필드 추가

---
+++ lib/controller/fetch_scroll/wish_scroll_controller.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:user_core2/controller/user_controller.dart';
+import 'package:user_core2/model/wish.dart';
+import 'package:user_core2/service/wish_service.dart';
+import 'package:user_core2/util/dialog.dart';
+
+class WishScrollController extends GetxController {
+  var scrollController = ScrollController().obs;
+  var data = <Wish>[].obs;
+  var isLoading = false.obs;
+  var hasMore = true.obs;
+  var page = 1.obs;
+  var total = 0.obs;
+
+  @override
+  void onInit() {
+    _getData();
+    scrollController.value.addListener(() {
+      if (scrollController.value.position.pixels ==
+              scrollController.value.position.maxScrollExtent &&
+          hasMore.value) {
+        _getData();
+      }
+    });
+    super.onInit();
+  }
+
+  Future<void> _getData() async {
+    try {
+      isLoading.value = true;
+      WishResponse response = await WishServices2.getWishes(
+          Get.find<UserController>().customer.value!.id, page.value);
+      isLoading.value = false;
+
+      if (response.status &&
+          response.data != null &&
+          response.data!.data.isNotEmpty) {
+        total.value = response.data!.total;
+        data.addAll(response.data!.data);
+        page.value++;
+      }
+      if (!response.status) {
+        showBasicAlertDialog(response.message);
+      }
+      if (!response.isDataHasMore) {
+        hasMore.value = false;
+      }
+    } catch (err) {
+      initFetchState();
+      showBasicAlertDialog('찜 목록 조회를 실패하였습니다.');
+    }
+  }
+
+  void initFetchState() {
+    isLoading.value = true;
+    data.clear();
+    hasMore.value = true;
+    page.value = 1;
+    total.value = 0;
+  }
+
+  Future<void> reload() async {
+    initFetchState();
+    _getData();
+  }
+}
--- lib/controller/wish_controller.dart
+++ lib/controller/wish_controller.dart
@@ -1,73 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:user_core2/controller/user_controller.dart';
 import 'package:user_core2/model/language.dart';
-import 'package:user_core2/model/wish.dart';
 import 'package:user_core2/service/wish_service.dart';
 import 'package:user_core2/util/dialog.dart';
 
 class WishController extends GetxController {
   var isLoading = false.obs;
   var isExist = false.obs;
-
-  var scrollController = ScrollController().obs;
-  var data = <Wish>[].obs;
-  var dataLoading = false.obs;
-  var hasMore = true.obs;
-  var page = 1.obs;
-  var total = 0.obs;
-
-  @override
-  void onInit() {
-    _getData();
-    scrollController.value.addListener(() {
-      if (scrollController.value.position.pixels ==
-              scrollController.value.position.maxScrollExtent &&
-          hasMore.value) {
-        _getData();
-      }
-    });
-    super.onInit();
-  }
-
-  Future<void> _getData() async {
-    try {
-      dataLoading.value = true;
-      WishResponse response = await WishServices2.getWishes(
-          Get.find<UserController>().customer.value!.id, page.value);
-      dataLoading.value = false;
-
-      if (response.status &&
-          response.data != null &&
-          response.data!.data.isNotEmpty) {
-        total.value = response.data!.total;
-        data.addAll(response.data!.data);
-        page.value++;
-      }
-      if (!response.status) {
-        showBasicAlertDialog(response.message);
-      }
-      if (!response.isDataHasMore) {
-        hasMore.value = false;
-      }
-    } catch (err) {
-      initFetchState();
-      showBasicAlertDialog('찜 목록 조회를 실패하였습니다.');
-    }
-  }
-
-  void initFetchState() {
-    dataLoading.value = true;
-    data.clear();
-    hasMore.value = true;
-    page.value = 1;
-    total.value = 0;
-  }
-
-  Future<void> reload() async {
-    initFetchState();
-    _getData();
-  }
 
   /* 찜하기 추가&제거 */
   Future<bool> toggleWish(productId) async {
